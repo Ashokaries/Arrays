282de07fbb868d5be001c85086edf44111707827
Ashokaries <12ash33@gmail.com>
2024-03-31 18:51:13 +0530

    Assigned to avalue X

--- src/ArraysPrograms/FrequencyOfNumInArray.java
+++ src/ArraysPrograms/FrequencyOfNumInArray.java
@@ -4,12 +4,12 @@ public class FrequencyOfNumInArray {
 
 	public static void main(String[] args) {
 		int a[] = { 1, 2, 3, 5, 5, 5, 5, 5, 5 };
-		int x = 5;
+		int x = 7;
 		int count = 0;
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] == x) {
 				count++;
-			}
+			} 	
 		}
 		System.out.println("frequency of 5 is occured :" + count + "times");
 
